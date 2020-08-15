4b5af1adf619ced7e258eb0e8a0007ddbb370388
Miroslav Stratiev <mstratiev@pop-os.localdomain>
2020-08-15 23:26:10 +0300

    Updated Shop Search. It will now work with multiple categories and keywords.

--- commands/subcommands/shop_search.js
+++ commands/subcommands/shop_search.js
@@ -1,6 +1,5 @@
 const SubCommand = require('../classes/subcommand');
 const utilites = require('../../utilities');
-const { Collection } = require('discord.js');
 
 class Shop_Search extends SubCommand {
 	async execute_internal(message, args) {
@@ -13,56 +12,146 @@ class Shop_Search extends SubCommand {
 			errors: ['time'],
 		};
 
-		message.channel.send('Enter category');
-		await message.channel
-			.awaitMessages(awaitFilter, awaitConditions)
-			.then(async (filteredCategory) => {
-				let categoryRow = db
-					.prepare(
-						'SELECT Category_id category_id FROM categories WHERE server_id = ? AND category_name = ?'
-					)
-					.get([message.guild.id, filteredCategory.first().content]);
+		const availableCategoriesRows = db
+			.prepare(
+				'SELECT * FROM categories WHERE server_id = ? ORDER BY category_name'
+			)
+			.all([message.guild.id]);
 
-				if (categoryRow) {
-					message.channel.send('Enter keyword');
-					await message.channel
-						.awaitMessages(awaitFilter, awaitConditions)
-						.then(async (filteredKeyword) => {
-							let itemRows = db
-								.prepare(
-									'SELECT * FROM items WHERE server_id = ? AND category_id = ? AND item_name LIKE ?'
-								)
-								.all([
-									message.guild.id,
-									categoryRow.category_id,
-									`%${filteredKeyword.first().content}%`,
-								]);
+		let selectedCategories = new Array(); // array of category ids
+		let selectedKeywords = new Array();
 
-							if (itemRows.size > 0) {
-								message.reply(
-									'I found these items matching your search criteria:'
-								);
-								itemRows.forEach((itemRow) => {
-									message.channel.send(itemRow.item_name);
-								});
-							} else {
-								message.reply(
-									'No items found that match your search criteria!'
-								);
-							}
-						});
+		let shopMessageCategories = async function () {
+			let result = 'Categories:\n';
+
+			availableCategoriesRows.forEach((availableCategoriesRow) => {
+				if (selectedCategories.includes(availableCategoriesRow.id)) {
+					result += `:white_check_mark: ${availableCategoriesRow.category_name}\n`;
 				} else {
-					message.reply('Invalid category');
-				}
-			})
-			.catch((error) => {
-				if (error instanceof Collection) {
-					// awaitMessages timed out
-					message.reply('Timed out');
-				} else {
-					throw new Error(error);
+					result += `:x: ${availableCategoriesRow.category_name}\n`;
 				}
 			});
+			return result;
+		};
+
+		let shopMessageKeywords = async function () {
+			let result = 'Keywords:\n';
+			result += selectedKeywords.join(', ');
+			return result;
+		};
+
+		// Let user select categories to search
+		message.reply('Enter categories:');
+
+		let awaiting = true;
+		while (awaiting) {
+			message.channel.send(await shopMessageCategories());
+
+			await message.channel
+				.awaitMessages(awaitFilter, awaitConditions)
+				.then(async (filteredCategoryName) => {
+					const categoryName = filteredCategoryName.first().content;
+
+					let matchedRow = availableCategoriesRows.find(
+						(availableCategoriesRow) => {
+							return (
+								availableCategoriesRow.category_name ===
+								categoryName
+							);
+						}
+					);
+
+					if (matchedRow) {
+						const selectedCategoryIndex = selectedCategories.indexOf(
+							matchedRow.id
+						);
+						if (selectedCategoryIndex > -1) {
+							selectedCategories.splice(selectedCategoryIndex, 1);
+						} else {
+							selectedCategories.push(matchedRow.id);
+						}
+					} else if (
+						categoryName === 'next' ||
+						categoryName === 'cancel'
+					) {
+						awaiting = false;
+					}
+				});
+		}
+
+		// Let user select keywords to search
+		message.reply('Enter keywords:');
+
+		awaiting = true;
+		while (
+			!selectedKeywords.includes('next') &&
+			!selectedKeywords.includes('cancel')
+		) {
+			message.channel.send(
+				(await shopMessageCategories()) +
+					'\n' +
+					(await shopMessageKeywords())
+			);
+
+			await message.channel
+				.awaitMessages(awaitFilter, awaitConditions)
+				.then(async (filteredKeyword) => {
+					const keyword = filteredKeyword.first().content;
+
+					const keywordIndex = selectedKeywords.indexOf(keyword);
+					if (keywordIndex > -1) {
+						selectedKeywords.splice(keywordIndex, 1);
+					} else {
+						selectedKeywords.push(keyword);
+					}
+				});
+		}
+
+		let matchedCategoryItemRows = db
+			.prepare(
+				'SELECT * FROM items WHERE server_id = ? AND category_id IN(?)'
+			)
+			.all([message.guild.id, selectedCategories.join(',')]);
+
+		// Create virtual table to use FTS
+		db.prepare('DROP TABLE IF EXISTS items_virtual').run();
+		db.prepare(
+			`CREATE VIRTUAL TABLE items_virtual USING FTS5(item_id, category_id, item_name, item_desc, item_price)`
+		).run();
+
+		// Populate the virtual table with the items that match the selected categories
+		matchedCategoryItemRows.forEach((itemRow) => {
+			db.prepare(
+				`INSERT INTO items_virtual(item_id, category_id, item_name, item_desc, item_price) VALUES(?,?,?,?,?)`
+			).run([
+				itemRow.id,
+				itemRow.category_id,
+				itemRow.item_name,
+				itemRow.item_description,
+				itemRow.item_price,
+			]);
+		});
+
+		// Match the items using the selected keywords
+		let matchedVirtualItems = db
+			.prepare(`SELECT * FROM items_virtual WHERE item_name MATCH ?`)
+			.all([selectedKeywords.join(' OR ')]);
+
+		if (matchedVirtualItems.length > 0) {
+			let foundItemsReply =
+				'I found these items matching your search criteria:\n';
+
+			matchedVirtualItems.forEach((virtualItem) => {
+				foundItemsReply += `${virtualItem.item_name}\n`;
+			});
+			foundItemsReply.trimRight();
+
+			message.reply(foundItemsReply);
+		} else {
+			message.reply(
+				"I couldn't find any items in the shop that match your search criteria!"
+			);
+		}
 	}
 }
 
