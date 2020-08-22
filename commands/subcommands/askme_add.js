e264eb254bee2fac980ca36d982b7f9b8242fd95
Miroslav Stratiev <mstratiev@pop-os.localdomain>
2020-08-22 20:43:14 +0300

    Update Askme, Askme Add and Askme Remove. Removed Askme Edit - Askme Add will now do the same thing.

--- commands/subcommands/askme_add.js
+++ commands/subcommands/askme_add.js
@@ -2,22 +2,110 @@ const utilities = require('../../utilities');
 
 const SubCommand = require('../classes/subcommand');
 class AskMe_Add extends SubCommand {
-	execute_internal(message, args) {
+	async execute_internal(message, args) {
 		let db = utilities.openDatabase();
-		let questionRow = db
-			.prepare(
-				'SELECT * FROM questions WHERE server_id = ? AND question = ?'
-			)
-			.get([message.guild.id, args[0]]);
+		let awaitingUserInput = true;
 
-		if (questionRow) {
-			message.reply('That question already exists!');
-		} else {
-			db.prepare(
-				'INSERT INTO questions(server_id, question, answer) VALUES(?,?,?)'
-			).run([message.guild.id, args[0], args.slice(1).join(' ')]);
+		// Message filter
+		const awaitFilter = (m) => m.author.id === message.author.id;
+
+		// Message conditions
+		const awaitConditions = {
+			idle: 1250,
+			max: 100,
+		};
+
+		// Message conditions
+		const awaitConditionsAnswer = {
+			max: 1,
+		};
+
+		let messagesToDelete = new Array();
+
+		const sendQuestionMessage = function () {
+			message
+				.reply(
+					'Enter the question you would like to add. Enter `finish` to finish adding questions.'
+				)
+				.then((m) => messagesToDelete.push(m));
+		};
+
+		sendQuestionMessage();
+		while (awaitingUserInput) {
+			await message.channel
+				.awaitMessages(awaitFilter, awaitConditions)
+				.then(async (filteredQuestions) => {
+					if (filteredQuestions.size > 0) {
+						let questionToAdd = filteredQuestions.first().content;
+						if (questionToAdd === 'finish') {
+							awaitingUserInput = false;
+						} else {
+							message
+								.reply('Enter answer for this question')
+								.then((r) => messagesToDelete.push(r));
+
+							await message.channel
+								.awaitMessages(
+									awaitFilter,
+									awaitConditionsAnswer
+								)
+								.then(async (filteredAnswer) => {
+									let questionRow = db
+										.prepare(
+											'SELECT * FROM questions WHERE server_id = ? AND question = ?'
+										)
+										.get([message.guild.id, questionToAdd]);
+
+									db.prepare(
+										`INSERT INTO questions(server_id, question, answer) VALUES(?,?,?)
+										ON CONFLICT(server_id, question) DO UPDATE SET answer=?`
+									).run([
+										message.guild.id,
+										questionToAdd,
+										filteredAnswer.first().content,
+										filteredAnswer.first().content,
+									]);
+
+									if (questionRow) {
+										message
+											.reply('Question updated!')
+											.then((r) =>
+												r.delete({ timeout: 3500 })
+											);
+									} else {
+										message
+											.reply('Question added!')
+											.then((r) =>
+												r.delete({ timeout: 3500 })
+											);
+									}
+
+									filteredAnswer.forEach((filteredMessage) =>
+										messagesToDelete.push(filteredMessage)
+									);
+								});
+						}
+
+						filteredQuestions.forEach((filteredMessage) =>
+							messagesToDelete.push(filteredMessage)
+						);
+
+						message.channel.bulkDelete(messagesToDelete);
+						messagesToDelete = new Array();
+
+						if (awaitingUserInput) {
+							sendQuestionMessage();
+						}
+					}
+				});
 		}
+
+		message
+			.reply('Finished adding questions!')
+			.then((r) => r.delete({ timeout: 4000 }));
+
+		db.close();
 	}
 }
 
-module.exports = new AskMe_Add('add', 'Desc', [2], ['Usage'], ['MANAGE_GUILD']);
+module.exports = new AskMe_Add('add', 'Desc', [0], ['Usage'], ['MANAGE_GUILD']);
--- commands/subcommands/askme_remove.js
+++ commands/subcommands/askme_remove.js
@@ -1,19 +1,116 @@
 const utilities = require('../../utilities');
+const { MessageEmbed } = require('discord.js');
 
 const SubCommand = require('../classes/subcommand');
 class AskMe_Add extends SubCommand {
-	execute_internal(message, args) {
+	async execute_internal(message, args) {
 		let db = utilities.openDatabase();
-		db.prepare(
-			'DELETE FROM questions WHERE server_id = ? AND question = ?'
-		).run([message.guild.id, args[0]]);
+		let awaitingUserInput = true;
+
+		// Message filter
+		const awaitFilter = (m) => {
+			let result = false;
+			if (m.author.id === message.author.id) {
+				if (!isNaN(parseInt(m.content)) || m.content === 'finish') {
+					result = true;
+				} else {
+					m.delete();
+				}
+			}
+			return result;
+		};
+
+		// Message conditions
+		const awaitConditions = {
+			idle: 1250,
+			max: 100,
+		};
+
+		let questionRows = new Array();
+		let messagesToDelete = new Array();
+
+		const sendQuestionsMessage = function () {
+			questionRows = db
+				.prepare(
+					'SELECT id, question, answer FROM questions WHERE server_id = ?'
+				)
+				.all(message.guild.id);
+
+			let messageIndex = 1;
+			const questionsEmbed = new MessageEmbed()
+				.setColor('#000000')
+				.setTitle(
+					'Enter the number of the question you want to delete. Enter `finish` to finish deleting questions.'
+				)
+				.setDescription(
+					questionRows
+						.map(
+							(questionRow) =>
+								`\`\`\`md\n${messageIndex++}. ${
+									questionRow.question
+								}\`\`\``
+						)
+						.join(' ')
+				);
+
+			message.channel
+				.send(questionsEmbed)
+				.then((m) => messagesToDelete.push(m));
+		};
+
+		sendQuestionsMessage();
+		while (awaitingUserInput) {
+			await message.channel
+				.awaitMessages(awaitFilter, awaitConditions)
+				.then(async (filteredQuestionIndex) => {
+					if (filteredQuestionIndex.size > 0) {
+						if (
+							filteredQuestionIndex.first().content === 'finish'
+						) {
+							awaitingUserInput = false;
+						} else {
+							let questionIndex = parseInt(
+								filteredQuestionIndex.first().content - 1
+							);
+
+							if (
+								questionIndex >= 0 &&
+								questionIndex < questionRows.length
+							) {
+								const questionRow = questionRows[questionIndex];
+								db.prepare(
+									'DELETE FROM questions WHERE id = ?'
+								).run([questionRow.id]);
+
+								message
+									.reply('Question deleted!')
+									.then((r) => r.delete({ timeout: 3500 }));
+							}
+						}
+						filteredQuestionIndex.forEach((filteredMessage) =>
+							messagesToDelete.push(filteredMessage)
+						);
+
+						message.channel.bulkDelete(messagesToDelete);
+						messagesToDelete = new Array();
+
+						if (awaitingUserInput) {
+							sendQuestionsMessage();
+						}
+					}
+				});
+		}
+
+		message
+			.reply('Finished deleting questions!')
+			.then((r) => r.delete({ timeout: 4000 }));
 	}
 }
 
 module.exports = new AskMe_Add(
 	'remove',
 	'Desc',
-	[1],
+	[0],
 	['Usage'],
 	['MANAGE_GUILD']
 );
