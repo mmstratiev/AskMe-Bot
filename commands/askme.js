39aa94a57eda85bf135d7b0e4edb79acb30d50f2
Miroslav Stratiev <mstratiev@pop-os.localdomain>
2020-08-22 17:10:19 +0300

    Updated Askme and Cart Checkout.

--- commands/askme.js
+++ commands/askme.js
@@ -4,6 +4,7 @@ const {
 	response_unknown_question,
 } = require('../localization.json');
 
+const { MessageEmbed } = require('discord.js');
 const { main_command } = require('../commands.json');
 const utilities = require('../utilities');
 
@@ -12,40 +13,124 @@ class AskMeCommand extends Command {
 	async execute_internal(message, args) {
 		let db = utilities.openDatabase();
 
-		let messageReply = '';
 		if (args.length == 0) {
 			// no arguments, display possible questions
-			messageReply = 'Sorry, it seems that I know nothing!';
-
-			let rows = db
+			let questionRows = db
 				.prepare(
-					'SELECT Question question FROM questions WHERE server_id = ?'
+					'SELECT question, answer FROM questions WHERE server_id = ?'
 				)
 				.all(message.guild.id);
 
-			if (rows.length > 0) {
-				messageReply =
-					'What would you like to know? You can ask me any of the following questions: \n';
+			if (questionRows.length > 0) {
+				let awaitingUserInput = true;
 
-				rows.forEach((row) => {
-					messageReply += `\`${row.question}\`\n`;
-				});
+				// Message filter
+				const awaitFilter = (m) => {
+					let result = false;
+					if (m.author.id === message.author.id) {
+						if (
+							!isNaN(parseInt(m.content)) ||
+							m.content === 'finish'
+						) {
+							result = true;
+						} else {
+							m.delete();
+						}
+					}
+					return result;
+				};
+
+				// Message conditions
+				const awaitConditions = {
+					idle: 1250,
+					max: 100,
+				};
+
+				let messagesToDelete = new Array();
+
+				const sendQuestionsMessage = function () {
+					let messageIndex = 1;
+					const questionsEmbed = new MessageEmbed()
+						.setColor('#000000')
+						.setTitle(
+							'What would you like to know? Enter the number of the question to get an answer. Enter `finish` to cancel command.'
+						)
+						.setDescription(
+							questionRows
+								.map(
+									(questionRow) =>
+										`\`\`\`md\n${messageIndex++}. ${
+											questionRow.question
+										}\`\`\``
+								)
+								.join(' ')
+						);
+
+					message.channel
+						.send(questionsEmbed)
+						.then((m) => messagesToDelete.push(m));
+				};
+
+				sendQuestionsMessage();
+				while (awaitingUserInput) {
+					await message.channel
+						.awaitMessages(awaitFilter, awaitConditions)
+						.then(async (filteredQuestionIndex) => {
+							if (filteredQuestionIndex.size > 0) {
+								if (
+									filteredQuestionIndex.first().content ===
+									'finish'
+								) {
+									awaitingUserInput = false;
+								} else {
+									let questionIndex = parseInt(
+										filteredQuestionIndex.first().content -
+											1
+									);
+
+									if (
+										questionIndex >= 0 &&
+										questionIndex < questionRows.length
+									) {
+										// Print out answer
+										const questionRow =
+											questionRows[questionIndex];
+										const answerEmbed = new MessageEmbed()
+											.setTitle(
+												`\`${questionIndex + 1}. ${
+													questionRow.question
+												}\``
+											)
+											.setDescription(
+												`\`\`\`yaml\n${questionRow.answer}\`\`\``
+											);
+										message.reply(
+											'You asked me...',
+											answerEmbed
+										);
+									}
+								}
+								filteredQuestionIndex.forEach(
+									(filteredMessage) =>
+										messagesToDelete.push(filteredMessage)
+								);
+
+								message.channel.bulkDelete(messagesToDelete);
+								messagesToDelete = new Array();
+
+								if (awaitingUserInput) {
+									sendQuestionsMessage();
+								}
+							}
+						});
+				}
+
+				message
+					.reply('Hope I answered all your questions!')
+					.then((r) => r.delete({ timeout: 4000 }));
+			} else {
+				message.reply('Sorry, it seems that I know nothing!');
 			}
-			message.reply(messageReply);
-		} else {
-			// one argument, try to answer question that matches argument
-			messageReply = response_unknown_question;
-			let row = db
-				.prepare(
-					'SELECT Answer answer FROM questions WHERE server_id = ? AND question = ?'
-				)
-				.get([message.guild.id, args[0]]);
-
-			if (row) {
-				messageReply = row.answer;
-			}
-
-			message.reply(messageReply);
 		}
 	}
 }
--- commands/subcommands/cart_checkout.js
+++ commands/subcommands/cart_checkout.js
@@ -15,116 +15,94 @@ class Cart_Checkout extends SubCommand {
 
 		if (cartRow) {
 			let cartItems = db
-				.prepare('SELECT * FROM cart_items WHERE cart_id = ?')
+				.prepare(
+					`SELECT items.id, items.item_name, items.item_description, items.item_price, cart_items.item_quantity
+					 FROM items 
+					 INNER JOIN cart_items 
+					 ON cart_items.item_id=items.id
+					WHERE cart_items.cart_id = ?`
+				)
 				.all([cartRow.id]);
 
 			if (cartItems.length > 0) {
-				// Item ID to Cart Item map
-				let itemIDToCartItem = new Map(
-					cartItems.map((cartItem) => [cartItem.item_id, cartItem])
-				);
-
-				// Get all items that match the ids
-				let itemsRows = db
-					.prepare(
-						'SELECT * FROM items WHERE id IN(SELECT item_id FROM cart_items WHERE cart_id = ?)'
-					)
-					.all([cartRow.id]);
-
 				await message
 					.reply(localization.reply_checkout_creating)
 					.then(async (r) => {
-						try {
-							let paypalItems = new Array();
-							itemsRows.forEach((itemRow) => {
-								paypalItems.push(
-									new createOrder.PaypalItem(
-										itemRow.item_name,
-										itemRow.item_description,
-										itemRow.id,
-										itemRow.item_price,
-										itemIDToCartItem.get(
-											itemRow.id
-										).item_quantity
-									)
+						const paypalItems = cartItems.map(
+							(cartItem) =>
+								new createOrder.PaypalItem(
+									cartItem.item_name,
+									cartItem.item_description,
+									cartItem.id,
+									cartItem.item_price,
+									cartItem.item_quantity
+								)
+						);
+
+						await createOrder
+							.createOrder(
+								createOrder.buildRequestBody(paypalItems),
+								false
+							)
+							.then((createResponse) => {
+								db.prepare(
+									`INSERT OR REPLACE INTO payments(id, server_id, user_id, payment_status, payment_time) VALUES(?,?,?,?,DATETIME('now'))`
+								).run([
+									createResponse.result.id,
+									message.guild.id,
+									message.author.id,
+									createResponse.result.status,
+								]);
+								createResponse.result.links.forEach(
+									(element) => {
+										if (element.rel === 'approve') {
+											const currencyFormatter = new Intl.NumberFormat(
+												'en-US',
+												{
+													style: 'currency',
+													currency: 'USD',
+												}
+											);
+
+											// Construct Cart message as embed
+											const newContent = new MessageEmbed()
+												.setColor('#7289da')
+												.setTitle(
+													`__${localization.reply_checkout_pay_now}__`
+												)
+												.setDescription(
+													`${
+														localization.reply_cart_total
+													} **${currencyFormatter.format(
+														createResponse.result
+															.purchase_units[0]
+															.amount.value
+													)}**`
+												)
+												.setURL(element.href)
+												.addFields(
+													cartItems.map(
+														(cartItem) => {
+															return {
+																name: `\`${cartItem.item_name}\` x **${cartItem.item_quantity}**`,
+																value: currencyFormatter.format(
+																	cartItem.item_quantity *
+																		cartItem.item_price
+																),
+																inline: true,
+															};
+														}
+													)
+												);
+
+											r.edit(
+												`<@${message.author.id}>, ${localization.reply_checkout_link_ready}`,
+												newContent
+											);
+										}
+									}
 								);
 							});
-
-							await createOrder
-								.createOrder(
-									createOrder.buildRequestBody(paypalItems),
-									true
-								)
-								.then((createResponse) => {
-									db.prepare(
-										`INSERT OR REPLACE INTO payments(id, server_id, user_id, payment_status, payment_time) VALUES(?,?,?,?,DATETIME('now'))`
-									).run([
-										createResponse.result.id,
-										message.guild.id,
-										message.author.id,
-										createResponse.result.status,
-									]);
-									createResponse.result.links.forEach(
-										(element) => {
-											if (element.rel === 'approve') {
-												const currencyFormatter = new Intl.NumberFormat(
-													'en-US',
-													{
-														style: 'currency',
-														currency: 'USD',
-													}
-												);
-
-												// Construct Cart message as embed
-												const newContent = new MessageEmbed()
-													.setColor('#7289da')
-													.setTitle(
-														`__${localization.reply_checkout_pay_now}__`
-													)
-													.setDescription(
-														`${
-															localization.reply_cart_total
-														} **${currencyFormatter.format(
-															createResponse
-																.result
-																.purchase_units[0]
-																.amount.value
-														)}**`
-													)
-													.setURL(element.href)
-													.addFields(
-														itemsRows.map(
-															(itemRow) => {
-																const quantity = itemIDToCartItem.get(
-																	itemRow.id
-																).item_quantity;
-
-																return {
-																	name: `\`${itemRow.item_name}\` x **${quantity}**`,
-																	value: currencyFormatter.format(
-																		quantity *
-																			itemRow.item_price
-																	),
-																	inline: true,
-																};
-															}
-														)
-													);
-
-												r.edit(
-													`<@${message.author.id}>,`,
-													newContent
-												);
-											}
-										}
-									);
-								});
-						} catch (error) {
-							throw new Error(error);
-						}
-						// } finally {
-						// r.delete();
-						// }
 					});
 			} else {
 				message.reply(localization.reply_cart_empty);
